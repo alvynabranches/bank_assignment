82f3a7816654360e97d7e11e7f6a43e486bbc350
Alvyn Abranches <alvynabranches@gmail.com>
2022-11-25 09:33:15 +0530

    Updated configuration

--- app/db.py
+++ app/db.py
@@ -1,5 +1,13 @@
+import os
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root@db:3306/transaction_db")
+DATABASE_URL = os.environ.get("DATABASE_URL", "db")
+MYSQL_USER = os.environ.get("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "")
+MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE", "transaction_db")
+
+engine = create_engine(
+    f"mysql+pymysql://{MYSQL_USER}{':' if len(MYSQL_PASSWORD) == 0 else ''}{MYSQL_PASSWORD}@{DATABASE_URL}:3306/{MYSQL_DATABASE}"
+)
 meta = MetaData()
 conn = engine.connect()
