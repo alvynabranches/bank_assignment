84f4928ba6ee8aea1e87af8725044234c4dd3938
Alvyn Abranches <alvynabranches@gmail.com>
2022-11-26 09:38:26 +0530

    Updated configs

--- app/config.py
+++ app/config.py
@@ -1,7 +1,13 @@
+import os
 import asyncio
 
-KAFKA_BOOTSTRAP_SERVERS = [ "kafka:9093" ]
 KAFKA_TOPIC = "messages"
 
 ANNUAL_INC_COL = "annual_inc"
 loop = asyncio.get_event_loop()
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "db")
+MYSQL_USER = os.environ.get("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "alvyn")
+MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE", "transaction_db")
+KAFKA_BOOTSTRAP_SERVERS = os.environ.get("KAFKA_BOOTSTRAP_SERVERS", "").split(",")
--- app/db.py
+++ app/db.py
@@ -1,12 +1,8 @@
 import os
+import config
 from sqlalchemy import create_engine, MetaData
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "db")
-MYSQL_USER = os.environ.get("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "")
-MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE", "transaction_db")
-
-conn_url = f"mysql+pymysql://{MYSQL_USER}{'' if len(MYSQL_PASSWORD) == 0 else ':'}{MYSQL_PASSWORD}@{DATABASE_URL}:3306/{MYSQL_DATABASE}"
+conn_url = f"mysql+pymysql://{config.MYSQL_USER}{'' if len(config.MYSQL_PASSWORD) == 0 else ':'}{config.MYSQL_PASSWORD}@{config.DATABASE_URL}:3306/{config.MYSQL_DATABASE}"
 print("Connection URL:", conn_url)
 engine = create_engine(conn_url)
 
