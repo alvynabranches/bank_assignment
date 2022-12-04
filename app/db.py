2beb50b51de6ce9a074f139ce4cccaf942eac0ff
Alvyn Abranches <alvynabranches@gmail.com>
2022-12-04 20:17:35 +0530

    Removed print statement

--- app/db.py
+++ app/db.py
@@ -3,7 +3,6 @@ import config
 from sqlalchemy import create_engine, MetaData
 
 conn_url = f"mysql+pymysql://{config.MYSQL_USER}{'' if len(config.MYSQL_PASSWORD) == 0 else ':'}{config.MYSQL_PASSWORD}@{config.DATABASE_URL}:3306/{config.MYSQL_DATABASE}"
-print("Connection URL:", conn_url)
 engine = create_engine(conn_url)
 
 meta = MetaData()
