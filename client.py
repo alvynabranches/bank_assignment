4adbb956dfe3d878b8510a6d10e65519fdcc217b
Alvyn Abranches <alvynabranches@gmail.com>
2022-12-12 15:18:31 +0530

    Updated client code

--- client.py
+++ client.py
@@ -2,8 +2,20 @@ import json
 import requests
 import pandas as pd
 
-df = pd.read_csv("/home/alvynabranches/Downloads/accepted_2007_to_2018Q4.csv")
+path = "/home/alvynabranches/Downloads"
+df = pd.read_csv(f"{path}/rejected_2007_to_2018Q4.csv")
 data = df.to_dict("records")
 for d in data:
-    res = requests.post("http://localhost:5000/transaction", data=json.dumps(d))
+    data = {}
+    data["amount_requested"] = d["Amount Requested"]
+    data["application_date"] = d["Application Date"]
+    data["loan_title"] = d["Loan Title"]
+    data["risk_score"] = d["Risk_Score"]
+    data["debt_to_income_ratio"] = d["Debt-To-Income Ratio"]
+    data["zip_code"] = d["Zip Code"]
+    data["state"] = d["State"]
+    data["employment_length"] = d["Employment Length"]
+    data["policy_code"] = d["Policy Code"]
+    res = requests.post("http://localhost:5000/transaction", data=json.dumps(data))
     print(res)
+    input()
