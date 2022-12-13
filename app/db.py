78841ff8634b58f6f801dd7bee52392764398643
Alvyn Abranches <alvynabranches@gmail.com>
2022-12-14 04:22:54 +0530

    Updated app

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ import config
 from sqlalchemy import create_engine, MetaData
 
 conn_url = f"mysql+pymysql://{config.MYSQL_USER}{'' if len(config.MYSQL_PASSWORD) == 0 else ':'}{config.MYSQL_PASSWORD}@{config.DATABASE_URL}:3306/{config.MYSQL_DATABASE}"
-engine = create_engine(conn_url)
+engine = create_engine(conn_url, pool_size=20, max_overflow=20)
 
 meta = MetaData()
 conn = engine.connect()
