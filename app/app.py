9cbe08ec2e8d141bd7643e26185ff9d245f3f718
Alvyn Abranches <alvynabranches@gmail.com>
2022-12-14 02:35:09 +0530

    Updated app

--- app/app.py
+++ app/app.py
@@ -35,23 +35,44 @@ async def consume():
         auto_offset_reset="earliest"
     )
     df = pd.DataFrame()
-    # try:
-    #     await consumer.start() # Only for AIOKafkaConsumer
-    for msg in consumer:
-        df = pd.concat([df, pd.DataFrame(msg)], axis=0, ignore_index=True)
-        MA50 = df[config.TARGET_COL].rolling(50).mean().tolist()[-1]
-        EMA50 = df[config.TARGET_COL].ewm(span=50, adjust=False).mean().tolist()[-1]
-        MA100 = df[config.TARGET_COL].rolling(100).mean().tolist()[-1]
-        conn.execute(transactions.insert().values(
-            **msg,
-            **{
-                f"{config.TARGET_COL}_MA50": MA50,
-                f"{config.TARGET_COL}_EMA50": EMA50,
-                f"{config.TARGET_COL}_MA100": MA100
-            }
-        ))
+    try:
+    	# await consumer.start() # Only for AIOKafkaConsumer
+		for msg in consumer:
+		    df = pd.concat([df, pd.DataFrame(msg)], axis=0, ignore_index=True)
+		    MA50 = df[config.TARGET_COL].rolling(50).mean().tolist()[-1]
+		    EMA50 = df[config.TARGET_COL].ewm(span=50, adjust=False).mean().tolist()[-1]
+		    MA100 = df[config.TARGET_COL].rolling(100).mean().tolist()[-1]
+		    conn.execute(transactions.insert().values(
+		        **msg,
+		        **{
+		            f"{config.TARGET_COL}_MA50": MA50,
+		            f"{config.TARGET_COL}_EMA50": EMA50,
+		            f"{config.TARGET_COL}_MA100": MA100
+		        }
+		    ))
+	except Exception as e:
+		print(e)
     # finally:
     #     await consumer.stop() # Only for AIOKafkaConsumer
+
+async def back(new_data):
+	all_transactions = transactions.all()
+	df = pd.DataFrame(all_transactions, columns=["id", "amount_requested", "application_date", "loan_title", "risk_score", "debt_to_income_ratio", "zip_code", "state", "employment_length", "policy_code", f"{config.TARGET_COL}_MA50", f"{config.TARGET_COL}_EMA50", f"{config.TARGET_COL}_MA100"])
+	new_record = pd.read_sql_query(query)
+	df = pd.concat([df, pd.DataFrame(new_data)], axis=0, ignore_index=True)
+	MA50 = df[config.TARGET_COL].rolling(50).mean().tolist()[-1]
+    EMA50 = df[config.TARGET_COL].ewm(span=50, adjust=False).mean().tolist()[-1]
+    MA100 = df[config.TARGET_COL].rolling(100).mean().tolist()[-1]
+    conn.execute(transactions.insert().values(
+        **msg,
+        **{
+            f"{config.TARGET_COL}_MA50": MA50,
+            f"{config.TARGET_COL}_EMA50": EMA50,
+            f"{config.TARGET_COL}_MA100": MA100
+        }
+    ))
+    conn.execute(transactions.insert(new_data))
+	
 
 @app.post("/transaction")
 async def transaction(message: RejectedData, request: Request, background: BackgroundTasks):
@@ -60,15 +81,18 @@ async def transaction(message: RejectedData, request: Request, background: Backg
         # loop=config.loop, # Only for AIOKafkaProducer
         bootstrap_servers=config.KAFKA_BOOTSTRAP_SERVERS
     )
-    # try:
-    #     await producer.start() # Only for AIOKafkaProducer
-    json_value = json.dumps(message.__dict__).encode("utf-8")
-    response = producer.send(topic=config.KAFKA_TOPIC, value=json_value)
-    conn.execute(transactions.insert(message.__dict__))
+    try:
+    	# await producer.start() # Only for AIOKafkaProducer
+    	json_value = json.dumps(message.__dict__).encode("utf-8")
+    	response = producer.send(topic=config.KAFKA_TOPIC, value=json_value)
+    	conn.execute(transactions.insert(message.__dict__))
+    except Exception as e:
+    	print(e)
     # finally:
     #     await producer.stop() # Only for AIOKafkaProducer
     # background.add_task(backgroundtask, message.id, client_host, client_port, "transaction")
     background.add_task(consume)
+    background.add_task(back, message.__dict__)
     return JSONResponse({"response": response}, 201)
 
 # asyncio.create_task(consume()) # Only for aiokafka module
